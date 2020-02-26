8cd3f7f6f2fed0932bbde86b417aab331dfe84b2
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-02-26 20:31:01 +1030

    lc_35_Search Insert Position

---
+++ lc_35_Search Insert Position/solution.py
@@ -0,0 +1,14 @@
+from _ast import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+
+        if nums[-1] <= target:
+            return len(nums)
+
+        for i in range(len(nums)):
+            if nums[i] > target:
+                return i
